8d87db070e56438fa7fbe0941132d3e85f4a29a9
Morgan Willock <77816378+morgan-willock@users.noreply.github.com>
2022-05-27 11:55:06 +0930

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { DEFAULT_THEME, ThemeProvider } from '@zendeskgarden/react-theming';
+import Example from './Example';
+
+const rootElement = document.getElementById('root');
+
+render(
+  <div style={{ padding: DEFAULT_THEME.space.md }}>
+    <ThemeProvider>
+      <Example />
+    </ThemeProvider>
+  </div>,
+  rootElement
+);
